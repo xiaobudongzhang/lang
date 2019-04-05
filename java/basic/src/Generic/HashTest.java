366b58d523244c2119a05a96103479630a3fdd1f
xiaobudongzhang <xiaobudongzhang@fangdd.com>
2019-04-05 10:17:13 +0800

    碰撞对比

--- java/basic/src/Generic/HashTest.java
+++ java/basic/src/Generic/HashTest.java
@@ -1,5 +1,8 @@
 package Generic;
 
+import java.util.HashMap;
+import java.util.Map;
+
 //测试hash>>> 16
 public class HashTest {
 
@@ -11,8 +14,19 @@ public class HashTest {
         System.out.println(Integer.toBinaryString(hashInt >>> 16));
 
         System.out.println(Integer.toBinaryString(hashInt ^ (hashInt >>> 16)));
-        //静态方法使用方式
+
         System.out.println(HashTest.hash(test));
+
+        //碰撞测试
+        Map<String,Object> user = new HashMap<>();//默认长度16
+        user.put("level", 1);
+        user.put("address", 2);
+        System.out.println("\"level\".hashCode() & (16 - 1)	:" + ("level".hashCode() & (16 - 1)));
+        System.out.println("\"address\".hashCode() & (16 - 1)	:" + ("address".hashCode() & (16 - 1)));
+
+        System.out.println("\"level\".hashCode() ^ \"level\".hashCode() >>> 16 & (16 - 1)	:" + (("level".hashCode() ^ ("level".hashCode() >>> 16) )& (16 - 1)));
+        System.out.println("\"address\".hashCode() ^ \"level\".hashCode() >>> 16 & (16 - 1)	:" + (("address".hashCode() ^ ("address".hashCode() >>> 16) ) & (16 - 1)));
+
     }
 
     static final int hash(Object key) {
