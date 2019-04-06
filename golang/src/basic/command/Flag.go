24f47a8139031c45773aa3578f890dd709ce9a24
xiaobudongzhang <xiaobudongzhang@fangdd.com>
2019-04-06 10:20:23 +0800

    flag

---
+++ golang/src/basic/command/Flag.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+var name string
+
+func init()  {
+	flag.StringVar(&name, "name", "zhang", "name max length 20")
+}
+
+func main()  {
+	//init的内容可以放到这里，必须放到Parse前
+	flag.Parse()//parse必须使用，否则取不到客户端传的数据
+	fmt.Println("input name is " + name)
+}
+
+
